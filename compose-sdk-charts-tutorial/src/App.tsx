8aea506d3817849bf074218b7561e8e8e3317723
Ahmed Raza <axa1874@student.bham.ac.uk>
2024-06-22 20:08:57 +0400

    Made changes to the main.tsx file to ignore the CORS setting

---
+++ compose-sdk-charts-tutorial/src/App.tsx
@@ -0,0 +1,175 @@
+import './App.css';
+import {
+  Chart,
+  ChartType,
+  DataPoint,
+  HighchartsOptions,
+} from '@sisense/sdk-ui';
+import { filterFactory, measureFactory } from '@sisense/sdk-data';
+import * as DM from './models/sample-retail';
+import { useMemo, useState } from 'react';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import BarChartIcon from '@mui/icons-material/BarChart';
+
+const productCategories = [
+  'Bikes',
+  'Body Armor',
+  'Build Kits',
+  'Cables & Housing',
+  'Frames',
+  'Helmets',
+  'Wheels & Wheelsets',
+];
+
+function App() {
+  const [categories, setCategories] = useState<string[]>([]);
+  const [breakBy, setBreakBy] = useState('color');
+  const [chartType, setChartType] = useState<ChartType>('column');
+  const chartFilters = useMemo(
+    () =>
+      categories
+        ? [filterFactory.members(DM.DimProducts.CategoryName, categories)]
+        : [],
+    [categories]
+  );
+
+  const handleCategoryChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    newCategories: string[]
+  ) => {
+    setCategories(newCategories);
+  };
+
+  const handleBreakChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    newBreak: string
+  ) => {
+    setBreakBy(newBreak);
+  };
+
+  const handleTypeChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    newType: ChartType
+  ) => {
+    if (newType) setChartType(newType);
+  };
+
+  const breakStringToColumn = (breakString: string) => {
+    if (breakString === 'manager') {
+      return DM.DimEmployees.TeamManger;
+    } else if (breakString === 'region') {
+      return DM.DimCountries.Region;
+    } else {
+      return DM.DimProducts.Color;
+    }
+  };
+
+  return (
+    <>
+      <ToggleButtonGroup value={categories} onChange={handleCategoryChange}>
+        {productCategories.map((productCategory) => (
+          <ToggleButton key={productCategory} value={productCategory}>
+            {productCategory}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+      <Chart
+        dataSet={DM.DataSource}
+        chartType={chartType}
+        dataOptions={{
+          category: [DM.DimProducts.CategoryName],
+          value: [
+            measureFactory.sum(
+              DM.Fact_Sale_orders.OrderRevenue,
+              'Total Revenue'
+            ),
+          ],
+        }}
+        highlights={chartFilters}
+        onBeforeRender={(options: HighchartsOptions) => {
+          if (options.tooltip) {
+            options.tooltip.formatter = function () {
+              const value = (
+                this.y && this.y !== 0 ? this.y / 1000 : 0
+              ).toFixed(1);
+              return `${this.x}: <b>${value}K</b>`;
+            };
+          }
+          return options;
+        }}
+        onDataPointClick={(point: DataPoint) => {
+          const category = point.categoryValue as string;
+          setCategories((categories) =>
+            categories.includes(category)
+              ? categories.filter((item) => item !== (category as string))
+              : [...categories, category as string]
+          );
+        }}
+        styleOptions={{
+          width: 1000,
+          height: 400,
+        }}
+      />
+      <ToggleButtonGroup
+        value={chartType}
+        onChange={handleTypeChange}
+        exclusive
+      >
+        <ToggleButton key={'column'} value={'column'}>
+          <BarChartIcon />
+        </ToggleButton>
+        <ToggleButton key={'bar'} value={'bar'}>
+          <BarChartIcon style={{ transform: 'rotate(90deg)' }} />
+        </ToggleButton>
+      </ToggleButtonGroup>
+      <Chart
+        dataSet={DM.DataSource}
+        chartType={chartType}
+        dataOptions={{
+          category: [DM.DimProducts.CategoryName],
+          value: [measureFactory.sum(DM.Fact_Sale_orders.OrderRevenue)],
+          breakBy: [breakStringToColumn(breakBy)],
+          seriesToColorMap: {
+            Black: '#1b1b1b',
+            Blue: '#0000cd',
+            Gold: '#fcc200',
+            Red: '#ce2029',
+            Silver: '#acacac',
+            Yellow: '#eee600',
+            USA: '#00008b',
+            Europe: '#dc143c',
+          },
+        }}
+        filters={chartFilters}
+        onBeforeRender={(options: HighchartsOptions) => {
+          if (options.tooltip) {
+            options.tooltip.formatter = function () {
+              const value = (
+                this.y && this.y !== 0 ? this.y / 1000 : 0
+              ).toFixed(1);
+              return `${this.x} - ${this.series.name}: <b>${value}K</b>`;
+            };
+          }
+          return options;
+        }}
+        styleOptions={{
+          width: 1000,
+          height: 400,
+        }}
+      />
+      <ToggleButtonGroup value={breakBy} onChange={handleBreakChange} exclusive>
+        <ToggleButton key={'color'} value={'color'}>
+          Color
+        </ToggleButton>
+        <ToggleButton key={'region'} value={'region'}>
+          Region
+        </ToggleButton>
+        <ToggleButton key={'manager'} value={'manager'}>
+          Team Manager
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </>
+  );
+}
+
+export default App;
